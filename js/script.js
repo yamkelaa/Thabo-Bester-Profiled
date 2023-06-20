b1311bfaa9ffc1bf03c5fb620175b7c4d96a6c02
yamkelaa <136578854+yamkelaa@users.noreply.github.com>
2023-06-20 16:05:46 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById("hamburger-menu")
+let dropDownIcon = document.getElementById("drop-down-menu");
+
+function menuToggle(){
+   menu.classList.toggle("isVisible");
+}
+
+console.log("Hello");
